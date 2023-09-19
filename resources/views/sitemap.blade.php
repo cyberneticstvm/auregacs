dcf21e12e4b29ec4dafcc05c361a8cac23be7bb3
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-09-19 16:44:35 +0530

    small updates

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,75 +1,73 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-        <loc>https://auregacs.com</loc>
-        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.1</priority>
-    </url>
-    <url>
-        <loc>https://auregacs.com/about</loc>
-        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.1</priority>
-    </url>
-    <url>
-        <loc>https://auregacs.com/services</loc>
-        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.1</priority>
-    </url>
-    <url>
-        <loc>https://auregacs.com/careers</loc>
-        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.1</priority>
-    </url>
-    <url>
-        <loc>https://auregacs.com/contact</loc>
-        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.1</priority>
-    </url>
-    <url>
-        <loc>https://auregacs.com/registration</loc>
-        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.1</priority>
-    </url>
-    <url>
-        <loc>https://auregacs.com/services/company-incorporation-and-trade-license</loc>
-        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.1</priority>
-    </url>
-    <url>
-        <loc>https://auregacs.com/services/golden-visa</loc>
-        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.1</priority>
-    </url>
-    <url>
-        <loc>https://auregacs.com/services/business-consulting</loc>
-        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.1</priority>
-    </url>
-    <url>
-        <loc>https://auregacs.com/services/accounting-and-book-keeping</loc>
-        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.1</priority>
-    </url>
-    <url>
-        <loc>https://auregacs.com/services/pro-service</loc>
-        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.1</priority>
-    </url>
-    <url>
-        <loc>https://auregacs.com/services/trademark-registration-and-intellectual-property</loc>
-        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.1</priority>
-    </url>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<!--  created with Free Online Sitemap Generator www.xml-sitemaps.com  -->
+<url>
+<loc>https://auregacs.com/</loc>
+<lastmod>2023-09-19T11:12:16+00:00</lastmod>
+<priority>1.00</priority>
+</url>
+<url>
+<loc>https://auregacs.com/about</loc>
+<lastmod>2023-09-19T11:12:16+00:00</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>https://auregacs.com/uae-free-zone</loc>
+<lastmod>2023-09-19T11:12:16+00:00</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>https://auregacs.com/uae-offshore</loc>
+<lastmod>2023-09-19T11:12:16+00:00</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>https://auregacs.com/uae-mainland</loc>
+<lastmod>2023-09-19T11:12:16+00:00</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>https://auregacs.com/services/company-incorporation-and-trade-license</loc>
+<lastmod>2023-09-19T11:12:16+00:00</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>https://auregacs.com/services/golden-visa</loc>
+<lastmod>2023-09-19T11:12:16+00:00</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>https://auregacs.com/services/business-consulting</loc>
+<lastmod>2023-09-19T11:12:16+00:00</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>https://auregacs.com/services/accounting-and-book-keeping</loc>
+<lastmod>2023-09-19T11:12:16+00:00</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>https://auregacs.com/services/pro-service</loc>
+<lastmod>2023-09-19T11:12:16+00:00</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>https://auregacs.com/services/trademark-registration-and-intellectual-property</loc>
+<lastmod>2023-09-19T11:12:16+00:00</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>https://auregacs.com/contact</loc>
+<lastmod>2023-09-19T11:12:16+00:00</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>https://auregacs.com/services</loc>
+<lastmod>2023-09-19T11:12:16+00:00</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>https://auregacs.com/careers</loc>
+<lastmod>2023-09-19T11:12:16+00:00</lastmod>
+<priority>0.80</priority>
+</url>
 </urlset>
