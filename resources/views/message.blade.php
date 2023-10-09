32942e22c2eafa57364d76a90a57287231488b34
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-10-09 18:39:35 +0530

    small updates

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -3,7 +3,7 @@
 <div class="container mt-5 pt-5">
     <div class="text-center">
         <h2 class="display-3">Message</h2>
-        <p class="display-5 text-danger mb-3">Thank you! Your meesage has been received successfullY.</p>
+        <p class="display-5 text-success mb-3">Thank you! Your meesage has been received successfullY.</p>
     </div>
 </div>
 @endsection
