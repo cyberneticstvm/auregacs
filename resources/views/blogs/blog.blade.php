de2c3fa0eb2b3e42e6706641cbc67a787538d743
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-10-18 17:39:44 +0530

    small updates

--- resources/views/blogs/blog.blade.php
+++ resources/views/blogs/blog.blade.php
@@ -3,6 +3,7 @@
 <div class="section">
     <div class="container">
         <h1 class="fw-light mb-3">{{ $blog->title }}</h1>
+        {!! $blog->content !!}
     </div><!-- end container -->
 </div>
 @endsection
